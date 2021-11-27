955312d2cac780c912dc13ab19260b324b8d4954
kevinRepos-droid <85376258+kevinRepos-droid@users.noreply.github.com>
2021-11-27 11:00:13 -0600

    creando login

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeNica from './screen/HomeNica';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola</Text>
-      <StatusBar style="auto" />
+     <HomeNica></HomeNica>
     </View>
   );
 }
